23b7b414c56a6e1332a3f98064e7ece4bf083558
klismannSis <kchancuana@unsa.edu.pe>
2023-05-19 23:02:49 -0500

    Completo

--- Bienvenido/HolaMundo.java
+++ Bienvenido/HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo{
 	public static void main(String args[]){
-		
+	
+	System.out.println("............................");	
 	System.out.println("Hola Mundo, Bienvenidos sean");
 	System.out.println("............................");
 
