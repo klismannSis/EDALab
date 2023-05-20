1913c984b5408748e2088dfc7cec8f2cd42d7737
klismannSis <kchancuana@unsa.edu.pe>
2023-05-19 23:24:49 -0500

    Corrigiendo Errores para Name

--- Bienvenido/Name.java
+++ Bienvenido/Name.java
@@ -1,5 +1,5 @@
 public class Name{
-	import java.util,Scanner;
+	import java.util.Scanner;
 	public static void main(String args[]){
 	
 	Scanner entrada = new Scanner(System.in);
@@ -7,3 +7,5 @@ public class Name{
 	System.out.println("Ingrese su nombre: ");
 	String name = entrada.nextLine();
 	System.out.println("Hola " + name );
+	}
+}
