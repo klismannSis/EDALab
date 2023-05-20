7b9364ee9a8e5774609c4cc697889476a7acf41b
klismannSis <kchancuana@unsa.edu.pe>
2023-05-19 23:22:09 -0500

    Commit para name

---
+++ Bienvenido/Name.java
@@ -0,0 +1,9 @@
+public class Name{
+	import java.util,Scanner;
+	public static void main(String args[]){
+	
+	Scanner entrada = new Scanner(System.in);
+
+	System.out.println("Ingrese su nombre: ");
+	String name = entrada.nextLine();
+	System.out.println("Hola " + name );
